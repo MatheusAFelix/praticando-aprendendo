707e5f7ce3c0124ee1ec1fae4681e57514e27fb8
Matheus A. Felix <126836977+MatheusAFelix@users.noreply.github.com>
2025-07-31 22:40:26 -0300

    Percorrendo Lista, usando for e while.

--- main.js
+++ main.js
@@ -1,5 +1,42 @@
-function tocaSomPom(){
-    document.querySelector('#som_tecla_pom').play();
+function tocaSom(idElementoAudio) {
+    document.querySelector(idElementoAudio).play();
 }
 
-document.querySelector('.tecla_pom').onclick = tocaSomPom;
+const listaDeTeclas = document.querySelectorAll('.tecla');
+
+let contador = 0;
+
+// //Enquanto, leght significa quantidade de teclas
+// while (contador < listaDeTeclas.length) {
+
+//     const tecla = listaDeTeclas[contador];
+//     const instrumento = tecla.classList[1];
+//     //template string
+//     const idAudio = `#som_${instrumento}`;
+
+//     // console.log(idAudio);
+
+//     //function anonima
+//     tecla.onclick = function () {
+//         tocaSom(idAudio);
+//     };
+
+//     contador = contador + 1;
+
+//     // console.log(contador);
+// }
+
+//Para, leght significa quantidade de teclas
+for (let contador = 0; contador < listaDeTeclas.length; contador ++) {
+
+    const tecla = listaDeTeclas[contador];
+    const instrumento = tecla.classList[1];
+    //template string
+    const idAudio = `#som_${instrumento}`;
+
+    //function anonima
+    tecla.onclick = function () {
+        tocaSom(idAudio);
+    };
+
+}
